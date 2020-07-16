0a375441a034fac82ba28942f5b8fcc10c4d27c4
rpolu <rpolu@192.168.1.101>
2020-07-16 09:07:46 +0530

    Committed

--- empjdbservetappLifecyle/src/com/elegant/sevlet/EmpServlet.java
+++ empjdbservetappLifecyle/src/com/elegant/sevlet/EmpServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
@@ -12,28 +13,35 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 public class EmpServlet implements Servlet {
+	Connection con = null;
+	PreparedStatement pst = null;
 
 	public void init(ServletConfig config) throws ServletException {
 
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "tiger");
+			String query = "insert into employee values(?,?,?)";
+			pst = con.prepareStatement(query);
+		} catch (Exception e) {
+		}
+		System.out.println("request came to init() method");
 	}
 
 	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+		System.out.println("request came to service() method");
+
 		String eid = req.getParameter("eid");
 		String name = req.getParameter("name");
 		String sal = req.getParameter("salary");
 
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "tiger");
 
-			String query = "insert into employee values(?,?,?)";
-			PreparedStatement pst = con.prepareStatement(query);
 			pst.setString(1, eid);
 			pst.setString(2, name);
 			pst.setString(3, sal);
 			pst.executeUpdate();
 			pst.close();
-			con.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -43,6 +51,15 @@ public class EmpServlet implements Servlet {
 	}
 
 	public void destroy() {
+		
+		System.out.println(" Servlet. Rip to sevlet");
+
+		try {
+			pst.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
